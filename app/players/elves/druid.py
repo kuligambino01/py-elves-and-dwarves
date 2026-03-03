c5b113688f71bc7873f7b77df6fd0a549ed9b5e0
kuligambino01 <czaro923@gmail.com>
2026-03-03 20:16:23 +0100

    'addingspace'

--- app/players/elves/druid.py
+++ app/players/elves/druid.py
@@ -12,7 +12,7 @@ class Druid(Elf):
 
     def player_info(self) -> str:
         return (f"Druid {self.nickname}. "
-                f"{self.nickname} has a favourite spell:"
+                f"{self.nickname} has a favourite spell: "
                 f"{self._favourite_spell}")
 
     def get_rating(self) -> int:
